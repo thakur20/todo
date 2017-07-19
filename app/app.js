0c3209ec73a2095bf04545ebfa7bcded8c1b7314
Siddhant Thakur <siddhant.thakur.20@gmail.com>
2017-07-19 12:40:54 +0530

    changes in controller path

--- app/app.js
+++ app/app.js
@@ -5,73 +5,20 @@ myToDoApp.config(['$stateProvider', function($stateProvider) {
     .state('home', {
       url: '',
       templateUrl: 'views/items.html',
-      controller: 'ToDoController'
+      controller: 'ToDoController',
+      controllerAs: 'todo'
     })
     .state('items', {
       url: '/items',
       templateUrl: 'views/items.html',
-      controller: 'ToDoController'
+      controller: 'ToDoController',
+      controllerAs: 'todo'
     })
     .state('itemdetails', {
       url: '/itemdetails',
       templateUrl: 'views/itemdetails.html',
-      controller: 'ToDoController'
-    })
-
-}]);
-
-myToDoApp.controller("ToDoController", ["$scope", function($scope) {
-  $scope.removeItem = function(items) {
-    var removeItem = $scope.todos.indexOf(items);
-    $scope.todos.splice(removeItem, 1);
-
-  };
-  $scope.additem = function() {
-    $scope.todos.push({
-      item: $scope.newitem.todoitem,
-      assignto: $scope.newitem.assignto,
-      date: $scope.newitem.date,
-      complete: false,
-      description: $scope.newitem.description
-    });
-    $scope.newitem.todoitem = "",
-      $scope.newitem.assignto = "",
-      $scope.newitem.date = "",
-      $scope.newitem.description = ""
-  };
-  $scope.removeAll = function() {
-    $scope.todos = [];
-  };
-
-  $scope.todos = [{
-      item: "Pay Wifi Bill",
-      assignto: "Raghav",
-      date: new Date("07/20/2017"),
-      complete: false,
-      description: "Pay WiFi bill for June and July month"
-
-    },
-    {
-      item: "Deposite Cash",
-      assignto: "Kapil",
-      date: new Date("05/06/2017"),
-      complete: false,
-      description: "Deposit Cash in A/C: XXXXXXX, bank: BOI, Account-holder:akshay"
-    },
-    {
-      item: "Improve Login Gui",
-      assignto: "sameer",
-      date: new Date("07/09/2017"),
-      complete: false,
-      description: "make the login screen responsive"
-    },
-    {
-      item: "book movie ticket",
-      assignto: "Aman",
-      date: new Date("06/30/2017"),
-      complete: false,
-      description: "book four movie ticket for evening show"
-    }
-  ];
+      controller: 'ToDoController',
+      controllerAs: 'todo'
+    })  
 
 }]);
